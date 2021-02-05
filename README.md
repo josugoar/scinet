aa1d2a228a6d7b7ffd8e77241b4f4474206b8bcd
Josu <56826352+josugoar@users.noreply.github.com>
2021-02-05 14:28:06 +0000

    Update project documentation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SCINET
+# scinet
 
 ![Build](https://img.shields.io/badge/build-passing-blue) ![Author](https://img.shields.io/badge/author-JoshGoA-green) ![License](https://img.shields.io/badge/license-MIT-red) ![PyPi](https://img.shields.io/badge/pypi-v0.4.9-yellow) ![Python](https://img.shields.io/badge/python->=3.8-orange)
 
--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="scinet-JoshGoA",
-    version="0.4.9",
-    author="JoshGoA",
+    name="scinet-josugoar",
+    version="0.5.0",
+    author="josugoar",
     description="Network science abstract data types",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/JoshGoA/scinet.git",
+    url="https://github.com/josugoar/scinet",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
