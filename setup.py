5a0f10bc5392e5ed0abdbabe190f4bd6e6a97278
Josu <56826352+josugoar@users.noreply.github.com>
2021-03-29 17:36:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="scinet-josugoar",
     version="0.5.0",
-    author="josugoar",
     description="Network science abstract data types",
+    author="josugoar",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/josugoar/scinet",
+    download_url="https://github.com/josugoar/scinet/archive/v0.5.0.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    keywords="algorithms data-structures docker graph library pipenv python",
+    project_urls={"Source": "https://github.com/josugoar/scinet"},
+    python_requires=">=3.8",
 )
